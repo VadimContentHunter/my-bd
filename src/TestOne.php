a4b3f0e164b9eeef87333f1a601c013ee7dafe9b
Vadim <project.k.vadim@gmail.com>
2022-12-29 12:42:24 +0200

    test 1

---
+++ src/TestOne.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyDB;
+
+/**
+ * MyDB
+ *
+ * @package   MyDB
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class MyDB
+{
+    public function myMethod(string $param): string
+    {
+        return '';
+    }
+}

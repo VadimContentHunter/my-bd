6a49107456b6eb02bf8e53b721ab5f04a59e534c
Vadim <project.k.vadim@gmail.com>
2023-01-03 16:52:58 +0200

    added SingleRequest class

---
+++ src/Requests/SingleRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyDB\Requests;
+
+use Psr\Log\NullLogger;
+use Psr\Log\LoggerInterface;
+use vadimcontenthunter\MyDB\RequestInterface;
+use vadimcontenthunter\MyDB\ConnectorInterface;
+use vadimcontenthunter\MyDB\SQLQueryBuilderInterface;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class SingleRequest implements RequestInterface
+{
+    protected string $query;
+
+    public function __construct(
+        protected ConnectorInterface $ConnectorInterface,
+        protected LoggerInterface $loggerInterface = new NullLogger()
+    ) {
+    }
+
+    public function singleQuery(SQLQueryBuilderInterface $query_builder): SingleRequest
+    {
+        return $this;
+    }
+
+    public function send(): mixed
+    {
+        return '';
+    }
+}

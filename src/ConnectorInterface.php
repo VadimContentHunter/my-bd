76fed2c71580511e76574c22ce48d5990e40e318
Vadim <project.k.vadim@gmail.com>
2023-01-03 16:40:31 +0200

    fixed Connector

---
+++ src/ConnectorInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyDB;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+interface ConnectorInterface
+{
+    public function connect(): PDO;
+}

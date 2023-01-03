2974e2491329b6e50c2bdc3ccbf06cdac509221b
Vadim <project.k.vadim@gmail.com>
2023-01-03 16:51:40 +0200

    added TransactionalRequests class

---
+++ src/Requests/TransactionalRequests.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyDB\Requests;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use vadimcontenthunter\MyDB\RequestInterface;
+use vadimcontenthunter\MyDB\ConnectorInterface;
+use vadimcontenthunter\MyDB\SQLQueryBuilderInterface;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class TransactionalRequests implements RequestInterface
+{
+    protected array $queries = [];
+
+    public function __construct(
+        protected ConnectorInterface $ConnectorInterface,
+        protected LoggerInterface $loggerInterface = new NullLogger()
+    ) {
+    }
+
+    public function addQuery(SQLQueryBuilderInterface $query_builder): TransactionalRequests
+    {
+        return $this;
+    }
+
+    public function send(): mixed
+    {
+        return '';
+    }
+}

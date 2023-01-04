bac5be972781f40faa3333a65ea5b3cd30991f6a
Vadim <project.k.vadim@gmail.com>
2023-01-04 22:49:07 +0200

    fixed return of values in methods

---
+++ src/MySQL/MySQLQueryBuilder/TableMySQLQueryBuilder/TableMySQLQueryBuilder.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyDB\MySQL\MySQLQueryBuilder\TableMySQLQueryBuilder;
+
+use vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\SQLQueryBuilder;
+use vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\TableSQLQueryBuilder\Operators\Operators;
+use vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\TableSQLQueryBuilder\TableSQLQueryBuilder;
+use vadimcontenthunter\MyDB\MySQL\MySQLQueryBuilder\TableMySQLQueryBuilder\Operators\MySqlOperators\MySqlOperators;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class TableMySQLQueryBuilder implements TableSQLQueryBuilder
+{
+    protected string $query = '';
+
+    public function create(string $table_name): Crea
+    {
+        return new MySqlOperators();
+    }
+
+    public function alter(string $field_name): Operators
+    {
+        return new MySqlOperators();
+    }
+
+    public function drop(string $table_name): TableSQLQueryBuilder
+    {
+        return $this;
+    }
+
+    public function rename(string $old_table_name, string $new_table_name): TableSQLQueryBuilder
+    {
+        return $this;
+    }
+
+    public function truncate(string $table_name): TableSQLQueryBuilder
+    {
+        return $this;
+    }
+
+    public function setQuery(string $query): SQLQueryBuilder
+    {
+        $this->query = $query;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuery(): string
+    {
+        return $this->query;
+    }
+}

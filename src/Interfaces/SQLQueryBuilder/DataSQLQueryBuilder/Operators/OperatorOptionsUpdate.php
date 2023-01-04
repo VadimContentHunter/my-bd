190a200f54aae83d8c486a84b51c67ad05c45720
Vadim <project.k.vadim@gmail.com>
2023-01-04 14:24:08 +0200

    fixed OperatorOptionsUpdate

--- src/Interfaces/SQLQueryBuilder/DataSQLQueryBuilder/Operators/OperatorOptionsUpdate.php
+++ src/Interfaces/SQLQueryBuilder/DataSQLQueryBuilder/Operators/OperatorOptionsUpdate.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\DataSQLQueryBuilder\Operators;
 
 use vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\SQLQueryBuilder;
+use vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\DataSQLQueryBuilder\Operators\Operators;
 
 /**
  * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>

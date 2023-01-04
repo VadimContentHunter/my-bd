0feda28beb23b65c91b92bb03858150f53df360b
Vadim <project.k.vadim@gmail.com>
2023-01-04 14:18:35 +0200

    added OperatorOptionsUpdate interface

---
+++ src/Interfaces/SQLQueryBuilder/DataSQLQueryBuilder/Operators/OperatorOptionsUpdate.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\DataSQLQueryBuilder\Operators;
+
+use vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\SQLQueryBuilder;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+interface OperatorOptionsUpdate extends SQLQueryBuilder
+{
+    public function set(string $field_name, mixed $value): OperatorOptionsUpdate;
+}

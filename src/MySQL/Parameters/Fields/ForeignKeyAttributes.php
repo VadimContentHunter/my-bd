fa59e40a053c6f84649457a8d69d71adc6f47643
Vadim <project.k.vadim@gmail.com>
2023-01-04 16:25:58 +0200

    added ForeignKeyAttributes class

---
+++ src/MySQL/Parameters/Fields/ForeignKeyAttributes.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyDB\MySQL\Parameters\Fields;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class ForeignKeyAttributes
+{
+    public const CASCADE = 'CASCADE';
+
+    public const SET_NULL = 'SET NULL';
+
+    public const RESTRICT = 'RESTRICT';
+
+    public const ON_DELETE = 'ON DELETE';
+
+    public const ON_UPDATE = 'ON UPDATE';
+
+    public static function constraint(string $name): string
+    {
+        return '';
+    }
+}

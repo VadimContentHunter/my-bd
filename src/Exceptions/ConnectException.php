44a9258f27894e6f0a4f30706266d1e8e3628972
Vadim <project.k.vadim@gmail.com>
2023-01-06 15:35:02 +0200

    Exceptions created

---
+++ src/Exceptions/ConnectException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyDB\Exception;
+
+use vadimcontenthunter\MyDB\Exception\MyDbException;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class ConnectException extends MyDbException
+{
+}
---
+++ src/Exceptions/MyDbException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\MyDB\Exception;
+
+/**
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class MyDbException extends \Exception
+{
+}

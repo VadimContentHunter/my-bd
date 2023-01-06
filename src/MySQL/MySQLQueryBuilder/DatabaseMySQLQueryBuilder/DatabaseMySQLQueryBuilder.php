31b6981cedb199f6d9bf67e24b864aca0b7a3fb7
Vadim <project.k.vadim@gmail.com>
2023-01-06 15:43:36 +0200

    added logic in DatabaseMySQLQueryBuilder class

--- src/MySQL/MySQLQueryBuilder/DatabaseMySQLQueryBuilder/DatabaseMySQLQueryBuilder.php
+++ src/MySQL/MySQLQueryBuilder/DatabaseMySQLQueryBuilder/DatabaseMySQLQueryBuilder.php
@@ -15,17 +15,31 @@ class DatabaseMySQLQueryBuilder implements DatabaseSQLQueryBuilder
 {
     protected string $query = '';
 
-    public function __construct(string $query)
-    {
-        $this->setQuery($query);
-    }
+    /**
+     * @param bool $exists - true, будет добавлена проверка БД "IF NOT EXISTS"
+     *                     - false, пытается создать базу данных, но если такая база данных уже существует, то операция возвратит ошибку.
+     */
     public function createDatabase(string $database_name, bool $exists = false): DatabaseSQLQueryBuilder
     {
+        if (!$exists) {
+            $this->query = 'CREATE DATABASE ' . $database_name;
+        } else {
+            $this->query = 'CREATE DATABASE IF NOT EXISTS ' . $database_name;
+        }
         return $this;
     }
 
+    /**
+     * @param bool $exists - true, будет добавлена проверка БД "IF EXISTS"
+     *                     - false, пытается удалить базу данных, но если такая база данных отсутствует на сервере, то операция возвратит ошибку.
+     */
     public function dropDatabase(string $database_name, bool $exists = false): DatabaseSQLQueryBuilder
     {
+        if (!$exists) {
+            $this->query = 'DROP DATABASE ' . $database_name;
+        } else {
+            $this->query = 'DROP DATABASE IF EXISTS ' . $database_name;
+        }
         return $this;
     }
 

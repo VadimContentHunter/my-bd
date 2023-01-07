7b427aa7f5df45e9cc1bc310360343c7d7a2e8f0
Vadim <project.k.vadim@gmail.com>
2023-01-07 23:32:05 +0200

    added tests for MySqlOperatorOptionsInsert class

--- tests/MySqlOperatorOptionsInsertTest.php
+++ tests/MySqlOperatorOptionsInsertTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace vadimcontenthunter\MyDB\Tests;
 
 use PHPUnit\Framework\TestCase;
+use vadimcontenthunter\MyDB\Exceptions\QueryBuilderException;
 use vadimcontenthunter\MyDB\Tests\Fakes\MySqlOperatorOptionsInsertFake;
 
 /**
@@ -24,10 +25,9 @@ class MySqlOperatorOptionsInsertTest extends TestCase
      * @test
      * @dataProvider providerAddValues
      *
-     * @param array<string,string[]|string> $data
-     * @param mixed $expected
+     * @param array<string,string[]> $data
      */
-    public function test_addValues_withParameters_mustAddValuesToParameter(array $data, mixed $expected): void
+    public function test_addValues_withParameters_shouldAddValuesToParameter(array $data, mixed $expected): void
     {
         foreach ($data as $field => $values) {
             foreach ($values as $key => $value) {
@@ -55,6 +55,76 @@ class MySqlOperatorOptionsInsertTest extends TestCase
                     'last_name' => ['Volkovskyi', 'Karasev', 'Trunevs'],
                     'age' => ['24', '25', '32'],
                 ],
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider providerGetQuery
+     *
+     * @param array<string,string[]> $data
+     */
+    public function test_getQuery_usingTheAddValuesMethod_shouldReturnASpecificString(array $data, mixed $expected): void
+    {
+        foreach ($data as $field => $values) {
+            foreach ($values as $key => $value) {
+                $this->mySqlOperatorOptionsInsertFake->addValues($field, $value);
+            }
+        }
+
+        $this->assertEquals($expected, $this->mySqlOperatorOptionsInsertFake->getQuery());
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    public function providerGetQuery(): array
+    {
+        return [
+            'test 1' => [
+                [
+                    'name' => ['Vadim', 'Sasha', 'Oleg'],
+                    'last_name' => ['Volkovskyi', 'Karasev', 'Trunevs'],
+                    'age' => ['24', '25', '32'],
+                ],
+                '(name,last_name,age) VALUES (Vadim,Sasha,Oleg), (Volkovskyi,Karasev,Trunevs), (24,25,32);',
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider providerGetQueryException
+     *
+     * @param array<string,string[]> $data
+     */
+    public function test_getQuery_usingTheAddValuesMethod_shouldReturnAnException(array $data, mixed $objException): void
+    {
+        $this->expectException(QueryBuilderException::class);
+
+        foreach ($data as $field => $values) {
+            foreach ($values as $key => $value) {
+                $this->mySqlOperatorOptionsInsertFake->addValues($field, $value);
+            }
+        }
+
+        $this->mySqlOperatorOptionsInsertFake->getQuery();
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    public function providerGetQueryException(): array
+    {
+        return [
+            'not a rectangular matrix' => [
+                [
+                    'name' => ['Vadim', 'Sasha', 'Oleg', 'Dima'],
+                    'last_name' => ['Volkovskyi', 'Karasev', 'Trunevs'],
+                    'age' => ['24', '25', '32'],
+                ],
+                new QueryBuilderException(),
             ],
         ];
     }

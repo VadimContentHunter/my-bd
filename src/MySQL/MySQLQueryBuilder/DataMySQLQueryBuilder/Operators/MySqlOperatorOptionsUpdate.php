d514d0ccfc1a869ecc863bbe8e6a4cf7c250b9a1
Vadim <project.k.vadim@gmail.com>
2023-01-08 20:10:15 +0200

    added logic in MySqlOperatorOptionsUpdate class

--- src/MySQL/MySQLQueryBuilder/DataMySQLQueryBuilder/Operators/MySqlOperatorOptionsUpdate.php
+++ src/MySQL/MySQLQueryBuilder/DataMySQLQueryBuilder/Operators/MySqlOperatorOptionsUpdate.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace vadimcontenthunter\MyDB\MySQL\MySQLQueryBuilder\DataMySQLQueryBuilder\Operators;
 
+use vadimcontenthunter\MyDB\Exceptions\QueryBuilderException;
 use vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\SQLQueryBuilder;
 use vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\DataSQLQueryBuilder\Operators\Operators;
 use vadimcontenthunter\MyDB\Interfaces\SQLQueryBuilder\DataSQLQueryBuilder\Operators\OperatorOptionsUpdate;
@@ -16,6 +17,10 @@ class MySqlOperatorOptionsUpdate implements OperatorOptionsUpdate
 {
     protected string $query = '';
 
+    /**
+     * @var array(string,string)
+     */
+    protected array $fieldsValues = [];
 
     public function setQuery(string $query): SQLQueryBuilder
     {
@@ -25,11 +30,43 @@ class MySqlOperatorOptionsUpdate implements OperatorOptionsUpdate
 
     public function getQuery(): string
     {
-        return $this->query;
+        return ($this->query[-1] ?? '') !== ';' ? $this->query . ';' : $this->query;
     }
 
-    public function set(string $field_name, mixed $value): OperatorOptionsUpdate|Operators
+    public function set(string $field_name, string|int $value, bool $wrapInQuotes = true): OperatorOptionsUpdate
     {
+        if ($wrapInQuotes) {
+            $this->fieldsValues[] = [$field_name => "'" . $value . "'"];
+        } else {
+            $this->fieldsValues[] = [$field_name => $value];
+        }
         return $this;
+    }
+
+    public function getOperators(): Operators
+    {
+        $this->query = $this->getFieldsValuesSQL();
+
+        $operators = new MySqlOperators();
+        $operators->setQuery($this->query);
+        return $operators;
+    }
+
+    /**
+     * @throws QueryBuilderException
+     */
+    protected function getFieldsValuesSQL(): string
+    {
+        if (count($this->fieldsValues) === 0) {
+            throw new QueryBuilderException("No values to update.");
+        }
+
+        $query = ' SET ';
+        foreach ($this->fieldsValues as $id => $value) {
+            $field_name = key($value) ?? throw new QueryBuilderException("Column not specified.");
+            $field_value = $value[$field_name];
+            $query .= $field_name . '=' . $field_value . ',';
+        }
+        return substr($query, 0, -1);
     }
 }

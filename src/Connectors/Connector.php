787fe4b69f804396b5df2d366981e28dbbe25393
Vadim <project.k.vadim@gmail.com>
2023-01-19 01:13:21 +0200

    added logic for connect method

--- src/Connectors/Connector.php
+++ src/Connectors/Connector.php
@@ -6,6 +6,7 @@ namespace vadimcontenthunter\Connectors;
 
 use PDO;
 use PDOException;
+use vadimcontenthunter\MyDB\Exceptions\ConnectException;
 use vadimcontenthunter\MyDB\Interfaces\ConnectorInterface;
 
 /**
@@ -23,19 +24,21 @@ class Connector implements ConnectorInterface
 
     public function __construct(
         public ?string $dsn = null,
+        public ?string $typeDb = null,
         public ?string $user = null,
         public ?string $password = null,
         public ?string $dbName = null,
-        public ?string $host = null
+        public ?string $host = null,
+        public ?string $port = null
     ) {
     }
 
     /**
      * @return array<array<string,mixed>>
      */
-    public function getOptions(): ?array
+    public function getOptions(): array
     {
-        return $this->options;
+        return $this->options ?? [];
     }
 
     /**
@@ -49,9 +52,27 @@ class Connector implements ConnectorInterface
 
     /**
      * @throws PDOException
+     * @throws ConnectException
      */
     public function connect(): PDO
     {
-        return new PDO('', '', '');
+        if ($this->dataBaseHost !== null) {
+            return $this->dataBaseHost;
+        }
+
+        $defaultDsn = null;
+
+        if (
+            $this->typeDb !== null
+            && $this->host !== null
+            && $this->dbName !== null
+        ) {
+            $port = $this->port !== null ? $this->port . ';' : '';
+            $defaultDsn .= $this->typeDb . ':host=' . $this->host . ';' . $port . $this->dbName;
+        }
+
+        $this->dsn = $this->dsn ?? ($defaultDsn ?? throw new ConnectException("Error, not enough data to connect."));
+
+        return new PDO($this->dsn, $this->user, $this->password, $this->getOptions());
     }
 }
